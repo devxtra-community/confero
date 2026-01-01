124df48636b5744751d1ba9b27f57792fedc5b94
AHAMMED FAVAZ K M <favazmubarak25@gmail.com>
2026-01-01 15:02:18 +0530

    chore: setup eslint, commitlint, husky, and build config

--- backend/src/config/morgan.ts
+++ backend/src/config/morgan.ts
@@ -1,5 +1,5 @@
 import morgan from 'morgan';
-import { logger } from './logger';
+import { logger } from './logger.js';
 
 export const morganMiddleware = morgan((tokens, req, res) => {
   logger.info({
