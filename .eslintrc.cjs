2aabdbb9fb45320a57bacf7d4c75a66397eda46e
AHAMMED FAVAZ K M <favazmubarak25@gmail.com>
2026-01-01 14:44:17 +0530

    chore: setup eslint and commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'no-console': 'off', // you are using Winston, so console is controlled
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+};
