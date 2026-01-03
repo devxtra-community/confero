7c7ce7218b5ce77d2bc045291b6519a93d70e118
sameer4712 <sameerptmc@gmail.com>
2026-01-03 16:00:55 +0530

    test: merge main with sameer

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { logger } from './logger.js';
 
 export const connection = async () => {
+  
   try {
     await mongoose.connect(process.env.MONGO_URI!);
     logger.info('Database connected successfully!');
